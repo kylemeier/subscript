8b058ae56da4877a47bf7c225ebd5c7535e07b40
Kyle Meier <kyledmeier@gmail.com>
2017-01-12 22:50:44 -0800

    form styling

--- src/components/FindPersonForm.jsx
+++ src/components/FindPersonForm.jsx
@@ -8,24 +8,32 @@ class FindPersonForm extends React.Component{
 	constructor(props){
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.shouldFocusInput = true;
 	}
 
 	handleSubmit(e){
 		e.preventDefault();
 		if(!this.input.value.trim()) return;
 		this.props.dispatch(fetchPeople(this.input.value));
-		this.input.value = '';
+		//not working
+		this.shouldFocusInput = false;
 	}
 	
 	render(){
 		return(
 			<form onSubmit={this.handleSubmit} className="FindPersonForm">
-			<input className="FindPersonForm-input"
-				ref={ node=>{ this.input = node }}
-			/>
-				<button className="FindPersonForm-btn" type="submit">
-					<i className="material-icons">search</i>
-				</button>
+			<h1 className="FindPersonForm-title">Welcome to Subscript</h1>
+			<div className="FindPersonForm-inputContainer">
+				<input 
+					className="FindPersonForm-input"
+					placeholder="Search for a movie cast/crew member..."
+					autoFocus={this.shouldFocusInput}
+					ref={ node=>{ this.input = node }}
+				/>
+					<button className="FindPersonForm-btn" type="submit">
+						<i className="material-icons">search</i>
+					</button>
+				</div>
 			</form>
 		);
 	}
