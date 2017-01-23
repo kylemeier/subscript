8da558462026e1589d79c32eb521a30dfe228954
Kyle Meier <kyledmeier@gmail.com>
2017-01-23 13:47:57 -0800

    dropdown styling and conditionally hiding

--- src/containers/FindPersonForm.jsx
+++ src/containers/FindPersonForm.jsx
@@ -1,69 +1,91 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import fetchPeople from '../actions/fetchPeople';
 import fetchMovies from '../actions/fetchMovies';
 import PeopleDropDown from './PeopleDropDown';
 import './FindPersonForm.css';
+import '../utils/material-icons-overrides.css';
 
-class FindPersonForm extends React.Component{
+class FindPersonForm extends React.Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props);
+		this.state = {
+			shouldShowDropDown: true
+		};
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.shouldFocusInput = true;
+		this.handleInputFocus = this.handleInputFocus.bind(this);
+		this.handleInputBlur = this.handleInputBlur.bind(this);
 		this.prevInputValue = '';
 	}
-	
-	componentDidMount(){
-		//should be debounced
+
+	componentDidMount() {
+		//should be debounced?
+		// throttle every half second?
 		// const interval = setInterval(()=>{
 		// 	this.fetchPeopleFromInput();
-		// }, 3000);
+		// }, 1000);
 	}
 
-	componentWillReceiveProps(nextProps){
+	componentWillReceiveProps(nextProps) {
 		const nextSelectedPersonID = nextProps.selectedPersonID;
 		const currentSelectedPersonID = this.props.selectedPersonID || null;
-		if(nextSelectedPersonID && nextSelectedPersonID !== currentSelectedPersonID) 
+		if (nextSelectedPersonID && nextSelectedPersonID !== currentSelectedPersonID)
 			this.props.dispatch(fetchMovies(nextSelectedPersonID));
 	}
 
-	handleSubmit(e){
+	handleInputFocus() {
+		this.setState({
+			shouldShowDropDown: true
+		});
+	}
+
+	handleInputBlur() {
+		this.setState({
+			shouldShowDropDown: false
+		});
+	}
+
+	handleSubmit(e) {
 		e.preventDefault();
 		this.fetchPeopleFromInput();
 	}
 
-	fetchPeopleFromInput(){
+	fetchPeopleFromInput() {
 		const newInputValue = this.input.value.trim();
 
-		if(!newInputValue || this.prevInputValue === newInputValue) return;
+		if (!newInputValue || this.prevInputValue === newInputValue) return;
 
 		this.props.dispatch(fetchPeople(newInputValue));
 		this.prevInputValue = newInputValue;
 	}
 
-	render(){
-		return(
+	render() {
+		return (
+
 			<form onSubmit={this.handleSubmit} className="FindPersonForm">
 				<div className="FindPersonForm-inputContainer">
-					<input 
-						className="FindPersonForm-input"
-						placeholder="Search for a movie cast/crew member..."
-						autoFocus={this.shouldFocusInput}
-						ref={ node=>{ this.input = node }}
-					/>
+					<div className="FindPersonForm-inputDropDownContainer">
+						<input
+							className="FindPersonForm-input"
+							placeholder="Search for a movie cast/crew member..."
+							onFocus={this.handleInputFocus}
+							onBlur={this.handleInputBlur}
+							ref={node => { this.input = node } }
+							/>
+						{ this.state.shouldShowDropDown && <PeopleDropDown /> }
+					</div>
 					<button className="FindPersonForm-btn" type="submit">
-						<i className="material-icons">search</i>
-					</button>	
+						<i className="material-icons km-material-icons--flex">search</i>
+					</button>
 				</div>
-				<PeopleDropDown />
 			</form>
 		);
 	}
 };
 
-const mapStateToProps = ({people})=>{
-	return{
+const mapStateToProps = ({people}) => {
+	return {
 		selectedPersonID: people.selectedPersonID,
 	};
 };
